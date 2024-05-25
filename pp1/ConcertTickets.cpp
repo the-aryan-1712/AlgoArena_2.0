6b016428e658f98c45f53006970fed755bee185a
the-aryan-1712 <156575544+the-aryan-1712@users.noreply.github.com>
2024-05-25 13:43:15 +0530

    Add files via upload

---
+++ pp1/ConcertTickets.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define test(t) int t;cin>>t;while(t--)
+#define int(t) int t;cin>>t
+#define MOD 1000000007
+#define vi vector<int>
+#define endl '\n' 
+#define PI 3.141592653589793238460
+#define ll(t) long long t;cin>>t
+#define fori(i, a, b) for (int i = a; i < b; i++)
+#define ford(i, a, b) for (int i = a; i > b; i--)
+
+int main()
+{   
+    ll(n);
+    ll(m);
+    multiset<long long>tp;
+    for(long long i=0;i<n;i++){
+        ll(x);
+        tp.insert(x);
+    }
+
+    vector<long long> mp(m);
+    for(long long i=0;i<m;i++){
+        
+        cin>>mp[i];
+    }
+    for(long long i=0;i<m;i++){
+        auto iter =  upper_bound(tp.begin(),tp.end(),mp[i]);
+        if(iter==tp.begin()){
+            cout<<-1<<endl;
+        }
+        else{
+            iter--;
+            cout<<(*iter)<<endl;
+            tp.erase(iter);
+        }
+        
+    
+    }
+
+
+
+    
+    return 0;
+}
